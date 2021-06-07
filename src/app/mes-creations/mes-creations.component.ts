82b08e37e0ea421a8e866295013591b3dc9d6bbb
Doucara Fodié <fodiedoucara8@gmail.com>
2021-06-07 16:08:00 +0200

    en cours

---
+++ src/app/mes-creations/mes-creations.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mes-creations',
+  templateUrl: './mes-creations.component.html',
+  styleUrls: ['./mes-creations.component.scss']
+})
+export class MesCreationsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
