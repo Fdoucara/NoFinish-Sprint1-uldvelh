67e8de77cc281f00133ba2d1be5f4d1931422719
gaelle1701 <gaellecoue@gmail.com>
2021-06-07 15:56:47 +0200

    add folder layout with nav-bar & footer & layout component, add home composent, add new logo and update app-routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './layout/nav-bar/nav-bar.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { LayoutComponent } from './layout/layout.component';
+import { HomeComponent } from './home/home.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    FooterComponent,
+    LayoutComponent,
+    HomeComponent
+ 
   ],
   imports: [
     BrowserModule,
